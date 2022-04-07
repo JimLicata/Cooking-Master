274caa47c35b479083bf09c82897684ae4fa0d6f
James Licata <jimmays.jl@gmail.com>
2022-04-07 17:57:00 -0400

    Setup Basic Scene
    Added players, vegetables, bounds, and basic UI

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = .05f;
+    public bool player1 = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player Controls
+        if (player1)
+        {
+            if (Input.GetKey("w"))
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+            }
+
+            if (Input.GetKey("s"))
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - speed);
+            }
+
+            if (Input.GetKey("d"))
+            {
+                transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+            }
+        }
+        else
+        {
+            if (Input.GetKey("i"))
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+            }
+
+            if (Input.GetKey("k"))
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - speed);
+            }
+
+            if (Input.GetKey("l"))
+            {
+                transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+            }
+
+            if (Input.GetKey("j"))
+            {
+                transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+            }
+        }
+    }
+}
