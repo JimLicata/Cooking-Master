d6ae1aa159cdfb2d9ca488888c786e6811c52a37
James Licata <jimmays.jl@gmail.com>
2022-04-11 23:35:58 -0400

    Minor asset swap
    Swapped plate sprite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,7 +28,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Application.persistentDataPath);
         if (!LoadGame())
         {
             GenerateTopScores();
